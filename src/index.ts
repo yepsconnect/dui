ef94235b07e310012091967bfa1cd95fc9dba90f
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2023-07-25 12:17:45 +0400

    fix: add component to components list

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "./assets/tailwind.css";
 
 // components
-import UIAlert from "./components/UIAlert.vue";
-import UIAvatar from "./components/UIAvatar.vue";
-import UIBadge from "./components/UIBadge.vue";
-import UIBreadcrumbs from "./components/UIBreadcrumbs.vue";
-import UIDrawer from "./components/UIDrawer.vue";
+import UIAlert from "@/components/UIAlert.vue";
+import UIAvatar from "@/components/UIAvatar.vue";
+import UIBadge from "@/components/UIBadge.vue";
+import UIBreadcrumbs from "@/components/UIBreadcrumbs.vue";
+import UIDrawer from "@/components/UIDrawer.vue";
 import UILoading from "@/components/UILoading.vue";
+import UIMenu from "@/components/UIMenu.vue";
 
 
 export { 
@@ -16,4 +17,5 @@ export {
   UIBreadcrumbs,
   UIDrawer,
   UILoading
+  UIMenu,
 };
